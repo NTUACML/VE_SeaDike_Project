d8e7cf02f908b711558e35e0373ccd9ab2629211
CK Chou <uu4avzd@hotmail.com>
2015-12-06 12:06:13 +0800

    Upload Module1

---
+++ Module1/Module1.h
@@ -0,0 +1,19 @@
+// Module1.h
+
+#pragma once
+
+using namespace System;
+
+namespace VE_SD {
+
+	public ref class Module1
+	{
+	public:
+		//Constructor
+		Module1();
+
+		//Public variable
+		int IniVar;
+	private:
+	};
+}
